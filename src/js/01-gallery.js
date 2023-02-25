f6dc4f9a90d8129b856421ec101d6f44f7c9191e
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-25 17:18:34 +0200

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,8 @@ import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from "simplelightbox";
 
+console.log(SimpleLightbox);
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
@@ -17,11 +19,10 @@ const createGallatyElements = galleryItems.map((item) => {
 
 gallery.insertAdjacentHTML('beforeend', createGallatyElements)
 
-
-/* 	var lightbox = new SimpleLightbox('.gallery a', {
+	var lightbox = new SimpleLightbox('.gallery a', {
 		captionType: 'attr',
 		captionsData: 'title',
 		captionPosition: 'bottom',
 		captionDelay: 250
-	}); */
+	});
 
