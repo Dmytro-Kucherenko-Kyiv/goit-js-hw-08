1e3e6275b7aac7f1a30ff84766ef05a88b4ad569
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-28 22:38:02 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,45 @@
-/* import throttle from "lodash.throttle"; */
+import throttle from "lodash.throttle";
 
 const STORAGE_KEY = "feedback-form-state";
-const storageValues = {};
 
 const refs = {
 	form: document.querySelector('.feedback-form'),
 	email: document.querySelector('input'),
 	message: document.querySelector('textarea')
-};
+}
 
 autocompleteForm();
 
-const handleInput = (event) => {
-	storageValues[event.target.name] = event.target.value;
-	localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValues));
-};
-
 const handleSubmit = (event) => {
 	event.preventDefault();
-	event.currentTarget.reset();
+
+	const formFields = event.target.elements;
+	console.log(formFields.email.value)
+
 	localStorage.removeItem(STORAGE_KEY);
-	console.log(storageValues);
+	localStorage.setItem(STORAGE_KEY, JSON.stringify({
+		email: formFields.email.value,
+		message: formFields.message.value
+	}));
+
+	if (refs.email.value === '' || refs.message.value === '') {
+		return alert("Please fill in all the fields!")
+	}
+
+	event.currentTarget.reset();
 };
 
-refs.form.addEventListener('input', handleInput);
-refs.form.addEventListener('submit', handleSubmit);
+refs.form.addEventListener('submit', throttle(handleSubmit, 500));
 
 function autocompleteForm() {
 	const savedMessage = localStorage.getItem(STORAGE_KEY);
 
 	const parsedMessage = JSON.parse(savedMessage);
 	
-	if (parsedMessage) {
-	refs.email.value = parsedMessage.email;
-	refs.message.value = parsedMessage.message;
-}
-}
+	if (savedMessage) {
+		refs.email.value = parsedMessage.email;
+		refs.message.value = parsedMessage.message;
+	}
 
-
+	console.log(savedMessage);
+}
